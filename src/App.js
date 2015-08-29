719e3e4fef3cd204c242247acd0baf90676e69a6
Oleg Akbarov <olegakbarov@gmail.com>
2015-08-29 16:24:12 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './main.styl';
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1>Hello, bitch.</h1>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, './build'),
+    filename: 'bundle.js',
+    publicPath: '/build'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot-loader', 'babel'],
+      include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.styl$/,
+      loader: 'style-loader!css-loader!stylus-loader'
+    }
+  ]
+  }
+};
