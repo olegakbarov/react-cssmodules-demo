36e6ed37c3211cddaf953851eda246d44336601f
Oleg Akbarov <olegakbarov@gmail.com>
2015-09-03 20:37:44 +0300

    removed uncool animations

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,43 +12,11 @@ export default class Header extends Component {
     window.removeEventListener('scroll', this.handleScroll);
   }
   state = {
-    topLeft: {},
-    topRight: {},
-    bottomLeft: {},
-    bottomRight: {},
     container: {}
   }
   handleScroll = () => {
     let scroll = window.document.body.scrollTop;
-    if (scroll > 70 && scroll < 100) {
-      this.setState({
-        topLeft: {
-          opacity: '0',
-          top: 0,
-          left: 0,
-          transform: 'rotate(0deg)'
-        },
-        topRight: {
-          opacity: '0',
-          top: 0,
-          right: 0,
-          transform: 'rotate(0deg)'
-        },
-        bottomLeft: {
-          opacity: '0',
-          bottom: 0,
-          left: 0,
-          transform: 'rotate(0deg)'
-        },
-        bottomRight: {
-          opacity: '0',
-          bottom: 0,
-          right: 0,
-          transform: 'rotate(0deg)'
-        }
-      });
-    }
-    if (scroll > 150) {
+    if (scroll > 50) {
       this.setState({
         container: {
           border: '3px solid white',
@@ -69,22 +37,6 @@ export default class Header extends Component {
             Build your first component with
           </div>
           <div style={this.state.container} styleName="container">
-            <div
-              style={this.state.topLeft}
-              styleName="block-top-left">
-            </div>
-            <div
-              style={this.state.topRight}
-              styleName="block-top-right">
-            </div>
-            <div
-              style={this.state.bottomLeft}
-              styleName="block-bottom-left">
-            </div>
-            <div
-              style={this.state.bottomRight}
-              styleName="block-bottom-right">
-            </div>
             <div
               style={this.state.text}
               styleName="block-text">
