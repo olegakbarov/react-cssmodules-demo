0e2ce4894a4fc27d8f526f1e17a436fd90e3d377
Oleg Akbarov <olegakbarov@gmail.com>
2015-09-06 16:28:58 +0300

    rebuild

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
 
   output: {
-    path: path.join(__dirname, './'),
+    path: path.join(__dirname, './build/'),
     filename: 'bundle.js',
     publicPath: '/build/'
   },
